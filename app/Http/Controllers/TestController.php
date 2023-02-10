5a4f6292ec8c923c59c811a3b9d58f4dacf1d561
erena0927 <ukyo.contact09@gmail.com>
2023-02-10 22:08:57 +0900

    test

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Test;
+
+class TestController extends Controller
+{
+    
+    public function index()
+    {
+        // Eloquent(エロくアント)
+        $values = Test::all();
+
+        $count = Test::count();
+
+        $first = Test::findOrFail(1);
+
+        $whereBBB = Test::where('text', '=', 'bbb');
+
+        dd($values, $count, $first, $whereBBB);
+
+        return view('tests.test', compact('values'));
+    }
+}
